e28c2b828f5e9a63334189ed62ef49ab696b6172
tobiamos <tobideveloper@gmail.com>
2017-11-04 05:47:32 +0000

    remove odd files

--- app.js
+++ app.js
@@ -8,15 +8,9 @@ const bodyParser = require('body-parser');
 
 require('./api/models/db');
 
-const index = require('./routes/index');
-const users = require('./routes/users');
 const apiRoutes = require('./api/routes/index');
 
 const app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -26,9 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
 app.use('/api', apiRoutes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
